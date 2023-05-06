489a426cbbb25706e6faeb80de099d98d2bc805e
liuyudong <yudongl@pacvue.com>
2023-05-06 21:23:21 +0800

    Initialize commit

--- src/main/java/com/liu/service/BloggerService.java
+++
@@ -1,9 +0,0 @@
-package com.liu.service;
-
-import com.liu.model.Blogger;
-
-public interface BloggerService {
-	public Blogger getByUsername(String username);
-	public Blogger getBloggerData();
-	public Integer updateBlogger(Blogger blogger);
-}
--- src/main/java/com/liu/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.liu.service;
-
-import java.util.List;
-
-import com.liu.model.User;
-
-public interface UserService {
-	public List<User> getAllUser(); 
-}
